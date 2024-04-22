c1a7d10b3fcbfcd5711405256ec67ac2e725d815
mikael-100 <maadmike@hotmail.se>
2024-04-22 11:21:15 +0200

    Styling data

--- we/src/components/ApiWeather.tsx
+++ we/src/components/ApiWeather.tsx
@@ -10,6 +10,17 @@ interface Props {
   };
 }
 
+interface vaderLista {
+  main: {
+    temp: number;
+  };
+  wind: {
+    speed: number;
+  };
+  dt_txt: string;
+  weather: weather[];
+}
+
 const ApiKey: string = import.meta.env.VITE_API_KEY;
 
 const ApiWeather: React.FC<Props> = ({ inputs }) => {
@@ -44,47 +55,81 @@ const ApiWeather: React.FC<Props> = ({ inputs }) => {
     {
       hamtaVader(inputs);
     }
+
     //   console.log(vader)
   }, [inputs]); // Lägg till inputs som ett beroende i useEffect
+
+  // Denna if kontrollerar om vader är null eller om vader.main är lika med null om detta är sant returneras loading...
+
+  if (!vader || vader.main === null) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
       <div>
-        <p>Hello</p>
-        {vader && (
+        <>
           <>
-            <>
-              <p>Plats: {vader.name}</p>
+            <div>
+              {vader.main &&
+                vader.sys &&
+                vader.weather &&
+                vader.wind &&
+                vader.weather[0] &&
+                vader.main.temp &&
+                vader.main.humidity &&
+                vader.name && (
+                  <>
+                    <p>Plats: {vader.name}</p>
 
-              {vader.weather && vader.weather[0] && (
-                <img
-                  src={`http://openweathermap.org/img/wn/${vader.weather[0].icon}.png`}
-                  alt="Weather Icon"
-                />
+                    <img
+                      src={`http://openweathermap.org/img/wn/${vader.weather[0].icon}.png`}
+                      alt="Weather Icon"
+                    />
+
+                    <p>Temp: {vader.main.temp}</p>
+                    <p>Vinstryrka: {vader.wind.speed}</p>
+                    <p>Luftfuktighet: {vader.main.humidity}</p>
+                    <p>
+                      Soluppgång:{" "}
+                      {new Date(vader.sys.sunrise * 1000)
+                        .toLocaleTimeString()
+                        .slice(0, -3)}
+                    </p>
+                    <p>
+                      Solnedgång:{" "}
+                      {new Date(vader.sys.sunset * 1000)
+                        .toLocaleTimeString()
+                        .slice(0, -3)}
+                    </p>
+                  </>
+                )}
+            </div>
+            <div>
+              {vader.list && (
+                <>
+                  <h1>{vader.city.name}</h1>
+                  {vader.list.map((item: vaderLista, index: number) => (
+                    <div className="flex mx-auto justify-center shadow border-10 border-black mt-4 bg-white-500 hover:bg-blue-200">
+                      <div>
+                        <div key={index} className="inline-block flex">
+                          <p>Temperatur: {item.main.temp}</p>
+                          <br />
+                          <p>Vindstyrka: {item.wind.speed}</p>
+                          <p>Datum: {item.dt_txt.slice(0, -6)}</p>
+                          <img
+                            alt=""
+                            src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </>
               )}
-
-              <p>Temp: {vader.main && vader.main.temp}</p>
-              <p>Vinstryrka: {vader.wind && vader.wind.speed}</p>
-              <p>Luftfuktighet: {vader.main && vader.main.humidity}</p>
-              <p>
-                Soluppgång:{" "}
-                {vader.ses &&
-                  vader.sys.sunrise &&
-                  new Date(vader.sys.sunrise * 1000)
-                    .toLocaleTimeString()
-                    .slice(0, -3)}
-              </p>
-              <p>
-                Solnedgång:{" "}
-                {vader.sys &&
-                  vader.sys.sunset &&
-                  new Date(vader.sys.sunset * 1000)
-                    .toLocaleTimeString()
-                    .slice(0, -3)}
-              </p>
-            </>
+            </div>
           </>
-        )}
+        </>
       </div>
     </>
   );
